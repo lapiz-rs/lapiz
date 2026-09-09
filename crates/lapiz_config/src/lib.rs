559c1eae56b1bcd3bb46d2c3dd542db59a9b7fc9
443eb9#C <443eb9@gmail.com>
2026-09-09 17:48:57 +0800

    config struct

--- crates/lapiz_config/src/lib.rs
+++ crates/lapiz_config/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{path::PathBuf, sync::LazyLock};
+
+use anyhow::Result;
+use directories::BaseDirs;
+use serde::{Serialize, de::DeserializeOwned};
+
+pub fn resolve_config_dir(name: &str) -> std::path::PathBuf {
+    static BASE_DIRS: LazyLock<Option<BaseDirs>> = LazyLock::new(BaseDirs::new);
+
+    let config_base = if let Ok(d) = std::env::var("CONFIG_DIR") {
+        PathBuf::from(d)
+    } else if let Some(d) = BASE_DIRS.as_ref() {
+        d.config_local_dir().to_path_buf()
+    } else {
+        std::env::current_exe().unwrap()
+    };
+
+    config_base.join(name)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait ConfigType: Serialize + DeserializeOwned {
+    const NAME: &'static str;
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct Config<T: ConfigType> {
+    value: T,
+}
+
+impl<T: ConfigType> Config<T> {
+    pub fn read() -> Result<Self> {
+        Ok(Self {
+            value: toml::from_str(&std::fs::read_to_string(resolve_config_dir(T::NAME))?)?,
+        })
+    }
+
+    pub fn write(&self) -> Result<()> {
+        std::fs::write(resolve_config_dir(T::NAME), toml::to_string(&self.value)?)?;
+        Ok(())
+    }
+
+    pub fn update(&mut self, f: impl FnOnce(&mut T)) -> Result<()> {
+        f(&mut self.value);
+        self.write()
     }
 }
