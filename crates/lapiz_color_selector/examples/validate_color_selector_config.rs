ff6299a9b0f6e0e6d966e47e670c15c6ca842cc2
443eb9#C <443eb9@gmail.com>
2026-09-11 16:53:40 +0800

    removed useless code

--- crates/lapiz_color_selector/examples/validate_color_selector_config.rs
+++
@@ -1,9 +0,0 @@
-use lapiz_color_selector::config::ColorSelectorConfigGroup;
-use lapiz_config::Configuration;
-
-fn main() {
-    let source = std::fs::read_to_string("target/configs/color_selector.toml").unwrap();
-    let config = ColorSelectorConfigGroup::parse(&source).unwrap();
-    assert_eq!(config.configs.len(), 3);
-    assert!(config.configs.iter().all(|config| config.planes.len() == 1));
-}
